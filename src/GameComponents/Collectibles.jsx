9f738b1e8d508405967b44be6efa4eb51ed8693a
AJ DeMarco <79189312+ajdemarco95@users.noreply.github.com>
2023-12-18 22:24:08 -0500

    fixed collectibles

--- src/GameComponents/Collectibles.jsx
+++ src/GameComponents/Collectibles.jsx
@@ -11,11 +11,11 @@ export default function Model(props) {
   const { nodes,  } = useGLTF('models/world/split/Collectibles.glb')
   return (
     <group {...props} dispose={null}>
-      <ChristmasTreeCookie position={[10.298, -17.887, 104.126]} rotation={[0,Math.PI ,0]} />
-      <ChristmasTreeCookie position={[-57.18, -17.887, 86.029]} rotation={[0,Math.PI * .5,0]}/>
-      <ChristmasTreeCookie position={[-81.854, -17.887, 91.397]} rotation={[0,Math.PI * .5,0]} />
-      <ChristmasTreeCookie position={[42.898, -18.49, 62.432]} rotation={[0,Math.PI * -.5 ,0]}/>
-      <ChristmasTreeCookie position={[-117.789, -3.275, 34.052]} rotation={[0,15,0]} />
+      <ChristmasTreeCookie position={[92.778, -17.887, -36.945]} rotation={[0,0 ,0]} />
+      <ChristmasTreeCookie position={[92.806, -17.887, 32.917]} rotation={[0,Math.PI * 1,0]}/>
+      <ChristmasTreeCookie position={[104.392, -17.887, 55.354]} rotation={[0,Math.PI * 1,0]} />
+      <ChristmasTreeCookie position={[44.054, -18.49, -57.612]} rotation={[0,0,0]}/>
+      <ChristmasTreeCookie position={[58.333, -3.275, 104.935]} rotation={[0,Math.PI,0]} />
 
       {/* <mesh name="collectible_1" geometry={nodes.collectible_1.geometry} material={materials['Material.003']} position={[10.298, -17.887, 104.126]} /> */}
        {/* <mesh name="collectible_1001" geometry={nodes.collectible_1001.geometry} material={materials['Material.005']} position={[-57.18, -17.887, 86.029]} />
--- src/MerryChristmasText.jsx
+++ src/MerryChristmasText.jsx
@@ -3,15 +3,15 @@ import { Text3D } from "@react-three/drei";
 function MerryChristmasText() {
   return (
     <Text3D
-        scale={2}
-        position={[-35, -2, 5.973]}
-        rotation={[-Math.PI, 2, -Math.PI]}
-        font={"./font/Bebas_Neue_Regular.json"}
-      >
-        Merry Christmas
-        <meshBasicMaterial color={"black"} />
-      </Text3D>
-  )
+      scale={2}
+      position={[0, -2, 50]}
+      rotation={[0, Math.PI, 0]}
+      font={"./font/Bebas_Neue_Regular.json"}
+    >
+      Merry Christmas
+      <meshBasicMaterial color={"black"} />
+    </Text3D>
+  );
 }
 
-export default MerryChristmasText
+export default MerryChristmasText;
